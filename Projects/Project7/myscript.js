f6277d81b3acf5afb94de46f4f00ae30d8ee013a
zwood348 <zwood348@gmail.com>
2025-12-02 19:14:30 -0700

    Added Project 7

---
+++ Projects/Project7/myscript.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+
+	logo1 = new Image
+	logo2 = new Image
+if (document.images) {
+	logo1.src = "images/logo1.gif"
+	logo2.src = "images/logo2.gif"
+}
+
+
+		var imgArray = new Array(
+			'image1lg.jpg',
+			'image2lg.jpg',
+			'image3lg.jpg',
+			'image4lg.jpg'
+			 
+
+		);
+
+
+		var titleArray = new Array(
+			'Couple Biking',
+			'Couple Cooking',
+			'Man Climbing',
+			'Man Snowboarding'
+			
+		);
+			
+		var imgPath = "images/fullsize/";
+		
+		function swapImage(imgID) {
+
+			var theImage = document.getElementById('theImage');
+			var textDiv = document.getElementById('bottomText');
+
+			var newImg;
+			var textTitle;
+
+			newImg = imgArray[imgID];
+			theImage.src = imgPath + newImg;
+
+			textTitle=titleArray[imgID];
+
+				textDiv.innerHTML = textTitle;
+		}
+			
+		function preloadImages() {		
+			for(var i = 0; i < imgArray.length; i++) {
+				var tmpImg = new Image;
+				tmpImg.src = imgPath + imgArray[i];
+			}
+		}
